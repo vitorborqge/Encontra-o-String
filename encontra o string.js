de8042973371afda92dc3083303168b4dfc11b4b
Vitor Borges Vieira <101013550+vitorborqge@users.noreply.github.com>
2022-07-12 15:34:42 -0300

    Add files via upload

---
+++ encontra o string.js
@@ -0,0 +1,15 @@
+// Criar um método para ler propriedades de um objeto e 
+// exibir somente as propriedades do tipo string que estão nesse objeto
+
+const filme = {
+    titulo : 'Vingadores',
+    ano: 2018,
+    diretor: 'Robin', 
+    personagem: 'Thor'
+}
+exibirProproedades(filme);
+function exibirPropriedades(obj) {
+        for (prop in obj)
+            if(typeof obj[prop] === 'string')
+                console.log(prop,obj[prop])
+}
